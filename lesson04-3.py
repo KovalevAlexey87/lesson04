51e97b1d60c66de45b4172d6f001c2b6d6b886e0
KovalevAlexey87 <alexey_kovalev87@mail.ru>
2022-04-20 11:01:32 +0300

    lesson04-3 доработка

--- lesson04-3.py
+++ lesson04-3.py
@@ -4,6 +4,4 @@
 Подсказка: использовать функцию range() и генераторное выражение.
 """
 
-numbers = range(20, 241)
-list = [el for el in numbers if el % 20 == 0 or el % 21 == 0]
-print(list)
+print([x for x in range(20, 241) if x % 20 == 0 or x % 21 == 0])
